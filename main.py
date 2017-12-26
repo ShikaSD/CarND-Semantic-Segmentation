1ab8c4f54a9be7a6595de13eb07fec6807358cd1
Andrei Shikov <andreiishikov@gmail.com>
2017-12-27 01:21:01 +0200

    Add base implementation

--- main.py
+++ main.py
@@ -32,8 +32,17 @@ def load_vgg(sess, vgg_path):
     vgg_layer3_out_tensor_name = 'layer3_out:0'
     vgg_layer4_out_tensor_name = 'layer4_out:0'
     vgg_layer7_out_tensor_name = 'layer7_out:0'
-    
-    return None, None, None, None, None
+
+    tf.saved_model.loader.load(sess, [vgg_tag], vgg_path)
+    graph = sess.graph
+
+    input     = graph.get_tensor_by_name(vgg_input_tensor_name)
+    keep_prob = graph.get_tensor_by_name(vgg_keep_prob_tensor_name)
+    layer3    = graph.get_tensor_by_name(vgg_layer3_out_tensor_name)
+    layer4    = graph.get_tensor_by_name(vgg_layer4_out_tensor_name)
+    layer7    = graph.get_tensor_by_name(vgg_layer7_out_tensor_name)
+
+    return input, keep_prob, layer3, layer4, layer7
 tests.test_load_vgg(load_vgg, tf)
 
 
@@ -46,8 +55,20 @@ def layers(vgg_layer3_out, vgg_layer4_out, vgg_layer7_out, num_classes):
     :param num_classes: Number of classes to classify
     :return: The Tensor for the last layer of output
     """
-    # TODO: Implement function
-    return None
+
+    pool3_out_scaled = tf.multiply(vgg_layer3_out, 0.0001, name='pool3_out_scaled')
+    pool4_out_scaled = tf.multiply(vgg_layer4_out, 0.01, name='pool4_out_scaled')
+    l2 = tf.contrib.layers.l2_regularizer(1e-3)
+
+    upscale_1 = tf.layers.conv2d_transpose(vgg_layer7_out, 512, 4, 2, padding='same', kernel_regularizer=l2)
+    upscale_1 = tf.add(upscale_1, pool4_out_scaled)
+
+    upscale_2 = tf.layers.conv2d_transpose(upscale_1, 256, 4, 2, padding='same', kernel_regularizer=l2)
+    upscale_2 = tf.add(upscale_2, pool3_out_scaled)
+
+    output = tf.layers.conv2d_transpose(upscale_2, num_classes, 16, 8, padding='same', kernel_regularizer=l2)
+
+    return output
 tests.test_layers(layers)
 
 
@@ -60,8 +81,14 @@ def optimize(nn_last_layer, correct_label, learning_rate, num_classes):
     :param num_classes: Number of classes to classify
     :return: Tuple of (logits, train_op, cross_entropy_loss)
     """
-    # TODO: Implement function
-    return None, None, None
+    logits = tf.reshape(nn_last_layer, (-1, num_classes))
+
+    loss = tf.nn.l2_loss(
+        tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=nn_last_layer, labels=tf.reshape(correct_label, (-1, num_classes)))))
+
+    train_op = tf.train.AdamOptimizer(learning_rate).minimize(loss)
+
+    return logits, train_op, loss
 tests.test_optimize(optimize)
 
 
@@ -80,8 +107,19 @@ def train_nn(sess, epochs, batch_size, get_batches_fn, train_op, cross_entropy_l
     :param keep_prob: TF Placeholder for dropout keep probability
     :param learning_rate: TF Placeholder for learning rate
     """
-    # TODO: Implement function
-    pass
+    sess.run(tf.global_variables_initializer())
+
+    for epoch in range(epochs):
+        loss = 0
+        for batch_image, batch_label in get_batches_fn(batch_size):
+            _, loss = sess.run([train_op, cross_entropy_loss], {
+                input_image  : batch_image,
+                correct_label: batch_label,
+                keep_prob    : 0.5,
+                learning_rate: 1e-4
+            })
+        print("EPOCH: {}, LOSS: {:.15f}".format(epoch, loss))
+
 tests.test_train_nn(train_nn)
 
 
@@ -99,6 +137,9 @@ def run():
     # You'll need a GPU with at least 10 teraFLOPS to train on.
     #  https://www.cityscapes-dataset.com/
 
+    correct_label = tf.placeholder(tf.float32, shape=[None, image_shape[0], image_shape[1], 2])
+    learning_rate = tf.placeholder(tf.float32, shape=[])
+
     with tf.Session() as sess:
         # Path to vgg model
         vgg_path = os.path.join(data_dir, 'vgg')
@@ -109,11 +150,29 @@ def run():
         #  https://datascience.stackexchange.com/questions/5224/how-to-prepare-augment-images-for-neural-network
 
         # TODO: Build NN using load_vgg, layers, and optimize function
+        input, keep_prob, layer3, layer4, layer7 = load_vgg(sess, vgg_path)
+        layer7 = tf.stop_gradient(layer7)
+        layer3 = tf.stop_gradient(layer3)
+        layer4 = tf.stop_gradient(layer4)
+        output = layers(layer3, layer4, layer7, num_classes)
+        logits, train_op, loss = optimize(output, correct_label, learning_rate, num_classes)
 
         # TODO: Train NN using the train_nn function
+        train_nn(
+            sess,
+            5,
+            8,
+            get_batches_fn,
+            train_op,
+            loss,
+            input,
+            correct_label,
+            keep_prob,
+            learning_rate
+        )
 
         # TODO: Save inference data using helper.save_inference_samples
-        #  helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input_image)
+        helper.save_inference_samples(runs_dir, data_dir, sess, image_shape, logits, keep_prob, input)
 
         # OPTIONAL: Apply the trained model to a video
 
