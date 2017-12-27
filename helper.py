3ae178312b0000d6e955416f572344c74541867b
Andrei Shikov <andreiishikov@gmail.com>
2017-12-27 23:58:21 +0200

    Add random augmentations

--- helper.py
+++ helper.py
@@ -91,6 +91,15 @@ def gen_batch_function(data_folder, image_shape):
                 gt_bg = gt_bg.reshape(*gt_bg.shape, 1)
                 gt_image = np.concatenate((gt_bg, np.invert(gt_bg)), axis=2)
 
+                if random.random() > .8:
+                    image *= 0.7
+
+                if random.random() > .7:
+                    angle = random.random() * 10.0
+                    if random.random() > .5:
+                        angle = -angle
+                    scipy.misc.imrotate(image, angle)
+
                 images.append(image)
                 gt_images.append(gt_image)
 
